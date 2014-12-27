71c66e5f194b7a4970e229b97b708de481f6a187
SotaYano <b1013061@fun.ac.jp>
2014-12-27 16:15:50 +0900

    Camera open

--- SmileHacks/src/com/example/smilehacks/CameraActivity.java
+++ SmileHacks/src/com/example/smilehacks/CameraActivity.java
@@ -1,25 +1,35 @@
 package com.example.smilehacks;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.hardware.Camera.Size;
 
 public class CameraActivity extends Activity {
+	
+	SurfaceView sur;
+	SurfaceHolder surHold;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_camera);
+		//getUIparts
 		final Button gallery = (Button) findViewById(R.id.gallery);
+		StartCamera();
 		gallery.setOnClickListener(new OnClickListener(){
-			public void onClick(View v){
-				startGalleryActivity();
-			}
+			public void onClick(View v){startGalleryActivity();}
 		});
 	}
 
@@ -32,6 +42,8 @@ public class CameraActivity extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+		
+
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
@@ -45,5 +57,83 @@ public class CameraActivity extends Activity {
 	private void startGalleryActivity(){
 		Intent intent = new Intent(this,GalleryActivity.class);
 		startActivity(intent);
+	}
+	private void StartCamera(){
+		sur = (SurfaceView) findViewById(R.id.surfaceView1);
+		surHold = sur.getHolder();
+		surHold.addCallback( new SurfaceCallback() );
+	}
+	private class SurfaceCallback implements SurfaceHolder.Callback{
+		
+		Camera c;
+		List<Size> cSize;
+		
+		public void surfaceCreated(SurfaceHolder holder){
+			c = Camera.open(0);
+			if(c != null){
+				try{
+					c.setPreviewDisplay(surHold);
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+				cSize = c.getParameters().getSupportedPreviewSizes();
+			}
+		}
+		
+		public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){
+			if(c != null){
+				System.out.println("helloworld2");
+				Parameters params = c.getParameters();
+				if(cSize != null){
+					System.out.println("helloworld3");
+					
+					
+					
+					final double ASPECT_TOLERANCE = 0.1;
+			        double targetRatio = (double) width / height;
+			            
+
+			        Size optimalSize = null;
+			        double minDiff = Double.MAX_VALUE;
+
+			        int targetHeight = height;
+
+			        // Try to find an size match aspect ratio and size
+			        for (Size size : cSize) {
+			            double ratio = (double) size.width / size.height;
+			            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE)
+			                continue;
+			            if (Math.abs(size.height - targetHeight) < minDiff) {
+			                optimalSize = size;
+			                minDiff = Math.abs(size.height - targetHeight);
+			            }
+			        }
+
+			        // Cannot find the one match the aspect ratio, ignore the requirement
+			        if (optimalSize == null) {
+			            minDiff = Double.MAX_VALUE;
+			            for (Size size : cSize) {
+			                if (Math.abs(size.height - targetHeight) < minDiff) {
+			                    optimalSize = size;
+			                    minDiff = Math.abs(size.height - targetHeight);
+			                }
+			            }
+			        }
+					
+			        
+					
+					
+					
+					params.setPreviewSize(optimalSize.width,optimalSize.height);
+	                c.setParameters(params);
+				}
+				c.startPreview();
+			}
+		}
+		public void surfaceDestroyed(SurfaceHolder arg0){
+			c.stopPreview();
+			c.release();
+			c = null;
+		}
 	}
 }
