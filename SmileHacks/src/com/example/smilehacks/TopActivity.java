6febadfd8269d0df6402d0eed09fc0736c6824cb
makky0620 <b1013059@gmail.com>
2014-12-27 14:06:51 +0900

    大丈夫

--- SmileHacks/src/com/example/smilehacks/TopActivity.java
+++ SmileHacks/src/com/example/smilehacks/TopActivity.java
@@ -4,13 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class TopActivity extends Activity {
 
+	Button bt1;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_top);
+		
+		bt1 = (Button) findViewById(R.id.button1);
 	}
 
 	@Override
