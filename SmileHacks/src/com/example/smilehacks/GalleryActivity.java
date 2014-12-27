fcaf870d4cd4998c052d6676bc617b3b91319bcb
makky0620 <b1013059@gmail.com>
2014-12-27 15:07:03 +0900

    galleryの変更

--- SmileHacks/src/com/example/smilehacks/GalleryActivity.java
+++ SmileHacks/src/com/example/smilehacks/GalleryActivity.java
@@ -1,34 +1,32 @@
 package com.example.smilehacks;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore.Images.Media;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class GalleryActivity extends Activity {
 
+	private ImageView imageView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_gallery);
+		Intent i = new Intent(
+	    Intent.ACTION_PICK, Media.EXTERNAL_CONTENT_URI);  
+		startActivityForResult(i, 0); 	
+		
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+		super.onActivityResult(requestCode, resultCode, data);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.gallery, menu);
-		return true;
-	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	
 }
